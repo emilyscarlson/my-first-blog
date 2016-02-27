f4f68aec83f2e97afdd85aa02938d6f262169482
emilyscarlson <emilyslatterycarlson@gmail.com>
2016-02-27 13:53:00 -0500

    end of part 15 of tutorial (pt. 2)

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+
+
+# version i had from first run through
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -21,3 +24,6 @@ urlpatterns = [
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'',include('blog.urls')),
 ]
+
+
+
